af5006ab2b2c54f6b1c01745d573a7ddbe74533c
toflo123 <toflokie11@aol.com>
2015-06-26 21:39:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Ldk7wgTAAAAAMUY6uNJsQZ9IfeR4fE2tpo-PVFn',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'https://toflo123.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
